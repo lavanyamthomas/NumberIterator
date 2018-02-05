9ffcbc7010c821beaae0f3924758db1a81a18e44
lavanyamthomas <lavanyamthomas@gmail.com>
2018-02-05 15:15:36 +1100

    Add the main java class

---
+++ src/main/java/com/afl/numberCheck/main/NumberCheck.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.afl.numberCheck.main;
+
+import java.util.Scanner;
+
+import com.afl.common.NumberHelper;
+import com.afl.numberCheck.service.DivisibleNumber;
+import com.afl.numberCheck.service.NumberIterateRule;
+
+/**
+ * @author lavanya
+ *
+ */
+public class NumberCheck {
+
+	/**
+	 * 
+	 */
+	public NumberCheck() {
+		
+	}
+
+	/**15
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		
+		NumberIterateRule rule = new DivisibleNumber(); 
+		System.out.println(rule.divisibleNumberIterateRule(NumberHelper.getNumberFromUser()));
+		
+	}
+	
+	
+
+}

417cdd1807f13996189bdc2e848daff62f5932f1
lavanyamthomas <lavanyamthomas@gmail.com>
2018-02-05 14:53:43 +1100

    Add service class(business logic)

---
+++ src/main/java/com/afl/numberCheck/service/DivisibleNumber.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.afl.numberCheck.service;
+
+import com.afl.common.NumberHelper;
+
+/**
+ * @author lavanya
+ *
+ */
+public class DivisibleNumber implements NumberIterateRule {
+
+	/**
+	 * 
+	 */
+	public DivisibleNumber() {		
+	}
+
+	public String  divisibleNumberIterateRule(Integer number) {
+		String startMsg = "The number " + number + " ";
+		String divMsg = startMsg + " can be divided by ";
+		String message = startMsg + "cannot be divided by 3 or 5.";
+		
+		Boolean DividedByThree = (number%NumberHelper.THREE ==0);
+		Boolean DividedByFive = (number%NumberHelper.FIVE ==0);
+		
+		if(DividedByThree && DividedByFive) {
+			return divMsg+ NumberHelper.THREE +" and " +NumberHelper.FIVE +"\n “BAZ”.";
+		}else if(DividedByThree) {
+			return divMsg+ NumberHelper.THREE +"\n “FOO”";
+		}else if(DividedByFive) {
+			return divMsg+ NumberHelper.FIVE +"\n “BAR”.";
+		}else		
+			return message;
+	}
+
+}
---
+++ src/main/java/com/afl/numberCheck/service/NumberIterateRule.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.afl.numberCheck.service;
+
+/**
+ * @author lavanya
+ *
+ */
+public interface NumberIterateRule {
+	
+	String divisibleNumberIterateRule(Integer number);
+}
